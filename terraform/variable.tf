138715c0834045153d0f4caf73308891555e9904
maciejewskimarcin <92676129+maciejewskimarcin@users.noreply.github.com>
2022-12-09 02:53:45 +0100

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,53 @@
+variable "bucketName" {
+  type    = string
+  default = "bucket50452"
+}
+
+variable "objectKey" {
+  type    = string
+  default = "test"
+}
+
+variable "aclType" {
+  type    = string
+  default = "private"
+}
+
+
+variable "indexWebsiteFile" {
+  type    = string
+  default = "index.html"
+}
+
+variable "errorWebsiteFile" {
+  type    = string
+  default = "error.html"
+}
+
+variable "awsSource" {
+  type    = string
+  default = "hashicorp/aws"
+}
+
+variable "awsVersion" {
+  type    = string
+  default = "~> 3.27"
+}
+
+variable "policyDocumentPrincipalsType" {
+  type    = string
+  default = "AWS"
+}
+
+variable "policyDocumentPrincipalsIdentifiers" {
+  type    = list(string)
+  default = ["1234567890"]
+}
+
+variable "policyDocumentPrincipalsActions" {
+  type    = list(string)
+  default = [
+    "s3:GetObject",
+    "s3:ListBucket",
+  ]
+}
